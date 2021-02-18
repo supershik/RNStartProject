28f2e1926c599d49e4776981eb5314920fe5e0e9
supershik <qingshan1029@hotmail.com>
2021-02-18 18:59:05 +0200

    tab slider

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,8 @@
-// Powered By Development Here
-// www.devhere.co
-// Terms: https://devhere.co/terms-and-conditions
-// Started in (28-05-2020)
+// https://www.youtube.com/watch?v=ZiSN9uik6OY&ab_channel=CatalinMiron
+
 import React from "react";
 import {
+  findNodeHandle,
   View,
   Text,
   StyleSheet,
@@ -14,6 +13,7 @@ import {
   Platform,
   FlatList,
   Dimensions,
+  Animated
 } from "react-native";
 import Theme from "../Theme";
 import { ScrollView } from "react-native-gesture-handler";
@@ -22,39 +22,157 @@ import { tranlateText } from "../translation/translate";
 import {useTheme} from '@react-navigation/native';
 import { useSelector } from "react-redux";
 
-const DEVICE_HEIGHT = Dimensions.get('window').height;
+const {width, height} = Dimensions.get('screen');
+
+const images = {
+  man:
+    'https://images.pexels.com/photos/3147528/pexels-photo-3147528.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  women:
+    'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  kids:
+    'https://images.pexels.com/photos/5080167/pexels-photo-5080167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  // skullcandy:
+  //   'https://images.pexels.com/photos/5602879/pexels-photo-5602879.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  // help:
+  //   'https://images.pexels.com/photos/2552130/pexels-photo-2552130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+};
+const data = Object.keys(images).map((i) => ({
+  key: i,
+  title: i,
+  image: images[i],
+  ref: React.createRef()
+}));
+
+const Tab = React.forwardRef(({item, onItemPress}, ref) => {
+  return (
+    <TouchableOpacity onPress={onItemPress}>
+      <View ref={ref} style={{flexDirection: 'row', justifyContent: 'space-evenly', alignItems: "center"}}>
+        <Image
+            source={{uri: item.image}}
+            style={{width: 20, height: 20, resizeMode: 'cover'}}
+          />
+        <Text style={{color: 'white', fontSize: 50/data.length, fontWeight: 'bold', textTransform: 'uppercase'}}>
+          {item.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+});
+
+
+const Indicator = ({measures, scrollX}) => {
+  const paddingHoz = 10
+  const inputRange = data.map((_, i) => i*width);
+  const indicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.width+paddingHoz*2),
+  })
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.x),
+  })
+
+  return (
+    <Animated.View 
+      style={{
+        position: 'absolute',
+        height: 36,
+        width: indicatorWidth,
+        left: -paddingHoz,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        transform: [{
+          translateX
+        }],
+        top: -5,
+        borderRadius: 8
+      }}
+    />
+  )
+}
+
+const Tabs = ({data, scrollX, onItemPress}) => {
+  const [measures, setMeasures] = React.useState([])
+  const containerRef = React.useRef()
+  React.useEffect(() => {
+    const m = []
+    data.forEach( item => {
+      item.ref.current.measureLayout(
+        containerRef.current, 
+        (x, y, width, height) => {
+          m.push({
+            x, 
+            y, 
+            width, 
+            height
+          })
+          if (m.length === data.length) {
+            setMeasures(m)
+          }
+        }
+      )
+    })
+  }, [])
+
+  return (
+    <View style={{position: 'absolute', top: 100, width}}>
+      <View 
+        ref={containerRef}
+        style={{justifyContent: 'space-evenly', flex: 1, flexDirection: 'row'}}>
+        {data.map((item, index) => {
+          return <Tab key={item.key} item={item} ref={item.ref} onItemPress={() => onItemPress(index)}/>
+        })}
+        {measures.length > 0 && <Indicator measures={measures} scrollX={scrollX}/>}
+      </View>
+    </View>
+  )
+}
 
 //login
-class Login extends React.Component {
-  render() {
-    const { config, colors } = this.props;
-    let myTheme = Theme;
-    myTheme = config.theme === 'dark' ? DarkTheme: Theme;
+export default function Login(props) {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const ref = React.useRef()
+  const onItemPress = React.useCallback(itemIndex => {
+    ref?.current?.scrollToOffset({
+      offset: itemIndex * width
+    })
+  })
 
-    return (
-      <View style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Text>
-            How are you
-          </Text>
-        </ScrollView>
-
+  return (
+    <View style={styles.container}>
+        <Animated.FlatList
+          ref={ref}
+          data={data}
+          keyExtractor={item => item.key}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: {x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          bounces={false}
+          renderItem={({item}) => {
+            return (
+              <View style={{width, height}}>
+                  <Image
+                    source={{uri: item.image}}
+                    style={{flex: 1, resizeMode: 'cover'}}
+                  />
+                  <View style={[StyleSheet.absoluteFillObject, {backgroundColor: 'rgba(0, 0, 0, 0.3)'}]} />
+              </View>
+            )
+          }}
+        />
+      <Tabs data={data} scrollX={scrollX} onItemPress={onItemPress} />
       </View>
     );
-  }
 }
 
-export default function(props) {
-  const {colors} = useTheme();
-  const config = useSelector((state) => state.config);
-  return <Login {...props} colors={colors} config={config}/>;
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Theme.white,
-    paddingHorizontal: 20,
+    backgroundColor: Theme.blue,
   },
   logoWrapper: {
     marginTop: 60,
